14f058b9f9cdee14dcc14ef3513713eae8088039
colinskeep <colinjinskeep@gmail.com>
2018-12-19 19:23:58 -0700

    linted

--- src/v1/routes/validators/getProfile.js
+++ src/v1/routes/validators/getProfile.js
@@ -1,9 +1,13 @@
 const Joi = require('joi');
+const regex =
+/^Bearer [a-zA-Z0-9]{1,}[\\.][a-zA-Z0-9]{1,}[\\.][a-zA-Z0-9-_]{1,}$/;
 
 module.exports = {
   validate: {
     headers: {
-      authorization: Joi.string().regex(/^Bearer [a-zA-Z0-9]{1,}[\.][a-zA-Z0-9]{1,}[\.][a-zA-Z0-9-_]{1,}$/).min(6).error(() => 'Invalid Authorization Key'),
+      authorization: Joi.string().regex(regex)
+          .min(6)
+          .error(() => 'Invalid Authorization Key'),
     },
   },
 };
