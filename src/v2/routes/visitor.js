5a5e53703c11a541bf5debc8b97d5c0b74edd16c
bkawk <email.will.in.china@gmail.com>
2019-02-04 09:30:51 +0800

    disable register

--- src/v2/routes/visitor.js
+++ src/v2/routes/visitor.js
@@ -3,13 +3,13 @@ const validate = require('express-validation');
 const visitorController = require('../controllers/visitor.js');
 const router = new express.Router();
 
-const postRegister = require('../validators/visitor/postRegister.js');
+// const postRegister = require('../validators/visitor/postRegister.js');
 const postReset = require('../validators/visitor/postReset.js');
 const postValidate = require('../validators/visitor/postValidate.js');
 const postPassword = require('../validators/visitor/postPassword.js');
 
-router.route('/register/').post(validate(postRegister.validate),
-    visitorController.postRegister);
+// router.route('/register/').post(validate(postRegister.validate),
+//     visitorController.postRegister);
 router.route('/validate/').post(validate(postValidate.validate),
     visitorController.postValidate);
 router.route('/password/').post(validate(postPassword.validate),
