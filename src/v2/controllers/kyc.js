46dfc934f53201f1e6877ed25ef7a54a71e07ea8
bitdeliver <steve.b@bitmodeler.com>
2019-02-19 13:37:45 -0500

    Minor comment addition...

--- src/v2/controllers/kyc.js
+++ src/v2/controllers/kyc.js
@@ -895,7 +895,7 @@ const NO_IMG=new Buffer('\x47\x49\x46\x38\x39\x61\x01\x00\x01\x00'+
                         '\x80\x00\x00\xff\xff\xff\x00\x00\x00\x2c'+
                         '\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02'+
                         '\x02\x44\x01\x00\x3b'
-                       ); // NO_IMG
+                       ); // a 1 X 1 pixel, gif...
 
 function getImage(req, res)
 {var email=null,
