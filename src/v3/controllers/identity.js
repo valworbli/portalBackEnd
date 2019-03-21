a02a945b847bbbdac667497f9a9bd22a5274c807
Valentin Ivanov <jira@ftpdaemon.net>
2019-03-21 03:17:51 +0200

    Added the POST /identity/image/ API call. Must add the tests too.

--- src/v3/controllers/identity.js
+++ src/v3/controllers/identity.js
@@ -1,10 +1,19 @@
+const HttpStatus = require('http-status-codes');
+const logger = require('../components/logger')(module);
+
 /**
  * POST identity/image
  * @param {string} req - The incoming request.
  * @param {string} res - The outcoming response.
  */
 function postImage(req, res) {
-
+  try {
+    logger.info('Successfully uploaded ' + req.files.length + ' files');
+    res.status(HttpStatus.OK).json({data: true});
+  } catch (err) {
+    logger.error('Error uploading the images: ' + JSON.stringify(err));
+    res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({data: false});
+  }
 }
 /**
  * GET identity/image
--- src/v3/routes/identity.js
+++ src/v3/routes/identity.js
@@ -2,6 +2,7 @@ const express = require('express');
 const validate = require('express-validation');
 const identityController = require('../controllers/identity.js');
 const router = new express.Router();
+// const jwtAuthenticator = require('../components/jwtAuthenticator');
 
 const postImage = require('../validators/identity/postImage.js');
 const getImage = require('../validators/identity/getImage.js');
@@ -9,14 +10,40 @@ const postApplication = require('../validators/identity/postApplication.js');
 const getApplication = require('../validators/identity/getApplication.js');
 const getDocuments = require('../validators/identity/getDocuments.js');
 
-router.route('/image/').get(validate(postImage.validate),
-    identityController.postImage);
-router.route('/image/').post(validate(getImage.validate),
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const aws = require('aws-sdk');
+
+aws.config.update({
+  secretAccessKey: process.env.SES_SECRET_ACCESS_KEY,
+  accessKeyId: process.env.SES_ACCESS_KEY_ID,
+  region: process.env.S3_IMAGES_BUCKET_REGION,
+});
+
+const s3 = new aws.S3();
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.S3_IMAGES_BUCKET_NAME,
+    metadata: function(req, file, cb) {
+      cb(null, {fieldName: file.fieldname});
+    },
+    key: function(req, file, cb) {
+      cb(null, Date.now().toString());
+    },
+  }),
+});
+
+router.route('/image/').get(validate(getImage.validate),
     identityController.getImage);
-router.route('/application/').get(validate(postApplication.validate),
-    identityController.postApplication);
-router.route('/application/').post(validate(getApplication.validate),
+router.route('/image/').post(validate(postImage.validate),
+    jwtAuthenticator({}), upload.array('photos', 3),
+    identityController.postImage);
+router.route('/application/').get(validate(getApplication.validate),
     identityController.getApplication);
+router.route('/application/').post(validate(postApplication.validate),
+    identityController.postApplication);
 router.route('/documents/').post(validate(getDocuments.validate),
     identityController.getDocuments);
 
