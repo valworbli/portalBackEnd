e86c38eb88079fddd2cb6360bd79c044d7b1a972
Valentin Ivanov <jira@ftpdaemon.net>
2019-04-23 13:18:46 +0300

    Corrected a linting error

--- src/v3/models/schemas/users.js
+++ src/v3/models/schemas/users.js
@@ -2,7 +2,6 @@ const Const = require('../../defs/const');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt-nodejs');
 const crypto = require('crypto');
-const logger = require('../../components/logger')(module);
 
 const identityImagesSchema = new mongoose.Schema({
   completed: {type: Boolean, index: true},
